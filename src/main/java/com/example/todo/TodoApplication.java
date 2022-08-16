a61a232c14442eec317fe47407c987757ec4a0b2
huy8895 <huytv.ous@mcredit.com.vn>
2022-08-16 10:56:44 +0700

    [VAADIN] INIT VAADIN v2 add layout

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,13 +1,24 @@
 package com.example.todo;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class TodoApplication {
+@Theme(value = "todo")
+@PWA(name = "My App todo", shortName = "My App todo", offlineResources = {})
+@Push
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+public class TodoApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
--- src/main/java/com/example/todo/views/MainLayout.java
+++ src/main/java/com/example/todo/views/MainLayout.java
@@ -1,27 +1,99 @@
 package com.example.todo.views;
 
+import com.example.todo.appnav.AppNav;
+import com.example.todo.appnav.AppNavItem;
+import com.example.todo.views.dashboard.DashboardView;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLink;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainLayout extends AppLayout {
-
+    private H1 viewTitle;
     public MainLayout() {
-        this.createHeader();
-        this.createDrawer();
         setPrimarySection(Section.DRAWER);
+        addToNavbar(true, this.createHeaderContent());
+        addToDrawer(this.createDrawerContent());
     }
 
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassNames("view-toggle");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("view-title");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("view-header");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("My App");
+        appName.addClassNames("app-name");
+
+        var section =
+                new com.vaadin.flow.component.html.Section(appName,
+                                                           this.createNavigation(),
+                                                           this.createFooter());
+        section.addClassNames("drawer-section");
+        return section;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("app-nav-footer");
+        // TODO: 16/8/2022 add user avatar login
+        Avatar avatar = new Avatar("huy8895", "https://randomuser.me/api/portraits/men/24.jpg");
+        avatar.addClassNames("me-xs");
+
+        ContextMenu userMenu = new ContextMenu(avatar);
+        userMenu.setOpenOnClick(true);
+        userMenu.addItem("Logout", e -> {
+            log.info("logout click!");
+        });
+        Span name = new Span("huy trịnh");
+        name.addClassNames("font-medium", "text-s", "text-secondary");
+
+        layout.add(avatar, name);
+        return layout;
+    }
+
+    private AppNav createNavigation() {
+        AppNav nav = new AppNav();
+        nav.addClassNames("app-nav");
+
+        nav.addItem(new AppNavItem("Dashboard", DashboardView.class, "la la-globe"));
+        nav.addItem(new AppNavItem("Todo", Todo.class, "la la-globe"));
+
+
+        return nav;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
 
     private void createHeader() {
 //        DrawerToggle toggle = new DrawerToggle();
