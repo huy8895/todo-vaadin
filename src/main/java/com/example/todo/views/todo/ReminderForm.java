17e17db9868064cbfdb4b0d89b7aac54f7b7f7ca
huy8895 <huytv.ous@mcredit.com.vn>
2022-08-18 17:52:32 +0700

    [VAADIN] create base reminder form #2

--- src/main/java/com/example/todo/views/todo/ReminderForm.java
+++ src/main/java/com/example/todo/views/todo/ReminderForm.java
@@ -2,41 +2,45 @@ package com.example.todo.views.todo;
 
 import com.example.todo.entity.Catelog;
 import com.example.todo.entity.Priority;
+import com.example.todo.entity.Reminder;
 import com.example.todo.entity.Tag;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
 public class ReminderForm extends FormLayout {
+    private Reminder reminder;
     TextField title = new TextField("Title");
     TextField notes = new TextField("Notes");
     EmailField url = new EmailField("URL");
-
     DatePicker datePicker = new DatePicker();
-
     TimePicker timePicker = new TimePicker();
-
-
     ComboBox<Tag> tags = new ComboBox<>("Tags");
     ComboBox<Catelog> list = new ComboBox<>("List");
     ComboBox<Priority> priority = new ComboBox<>("Priority");
-
     Button done = new Button("Done");
     Button cancel = new Button("Cancel");
+    Binder<Reminder> binder = new Binder<>(Reminder.class);
 
     public ReminderForm(List<Tag> tagList, List<Catelog> catelogs) {
         addClassName("reminder-form");
+        binder.bindInstanceFields(this);
         list.setItems(catelogs);
         list.setItemLabelGenerator(Catelog::getName);
         tags.setItems(tagList);
@@ -64,6 +68,55 @@ public class ReminderForm extends FormLayout {
         this.cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         this.done.addClickShortcut(Key.ENTER);
         this.cancel.addClickShortcut(Key.ESCAPE);
+
+        this.done.addClickListener(event -> validateAndSave());
+        this.cancel.addClickListener(event -> fireEvent(new CloseEvent(this)));
         return new HorizontalLayout(done, cancel);
+    }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(reminder);
+            fireEvent(new SaveEvent(this, reminder));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setReminder(Reminder reminder){
+        this.reminder = reminder;
+        binder.readBean(reminder);
+    }
+
+    public static abstract class ReminderFormEvent extends ComponentEvent<ReminderForm> {
+        private final Reminder reminder;
+
+        public ReminderFormEvent(ReminderForm source, Reminder reminder) {
+            super(source, false);
+            this.reminder = reminder;
+        }
+
+        public Reminder getReminder(){
+            return this.reminder;
+        }
+    }
+
+    public static class SaveEvent extends ReminderFormEvent {
+
+        public SaveEvent(ReminderForm source, Reminder reminder) {
+            super(source, reminder);
+        }
+    }
+
+    public static class CloseEvent extends ReminderFormEvent {
+        public CloseEvent(ReminderForm source) {
+            super(source, null);
+        }
+    }
+
+    @Override
+    protected <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                     ComponentEventListener<T> listener) {
+        return super.addListener(eventType, listener);
     }
 }
--- src/main/java/com/example/todo/views/todo/Todo.java
+++ src/main/java/com/example/todo/views/todo/Todo.java
@@ -1,7 +1,9 @@
 package com.example.todo.views.todo;
 
+import com.example.todo.entity.Reminder;
 import com.example.todo.repository.CatelogRepository;
 import com.example.todo.repository.TagRepository;
+import com.example.todo.service.ReminderService;
 import com.example.todo.views.MainLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -13,9 +15,12 @@ public class Todo extends VerticalLayout {
     ReminderForm reminderForm;
     private final TagRepository tagRepository;
     private final CatelogRepository catelogRepository;
-    public Todo(TagRepository tagRepository, CatelogRepository catelogRepository) {
+
+    private final ReminderService reminderService;
+    public Todo(TagRepository tagRepository, CatelogRepository catelogRepository, ReminderService reminderService) {
         this.tagRepository = tagRepository;
         this.catelogRepository = catelogRepository;
+        this.reminderService = reminderService;
         addClassName("todo-view");
         setSizeFull();
         configureForm();
@@ -25,6 +30,12 @@ public class Todo extends VerticalLayout {
     private ReminderForm configureForm() {
         reminderForm = new ReminderForm(tagRepository.findAll(), catelogRepository.findAll());
         reminderForm.setWidth("25em");
+        reminderForm.addListener(ReminderForm.SaveEvent.class, event -> this.saveReminder(event));
         return reminderForm;
+    }
+
+    private void saveReminder(ReminderForm.SaveEvent event) {
+        final var reminder = event.getReminder();
+        reminderService.save(reminder);
     }
 }
